bd7e96ad9b87aaf0009d61af34c69ce9f40b9f7a
Geonu Kim <kgo000926@gmail.com>
2023-12-09 17:07:09 +0900

    [#5] feat: 영화 예매

---
+++ src/main/java/jbnu/ssad1/repository/booking/BookingRepository.java
@@ -0,0 +1,20 @@
+package jbnu.ssad1.repository.booking;
+
+import jbnu.ssad1.medel.entity.Booking;
+
+import java.util.List;
+
+public interface BookingRepository {
+
+    Booking save(Booking booking);
+
+    Booking findById(Long bookingId);
+
+    List<Booking> findAll();
+
+    List<Booking> findByMemberId(Long memberId);
+
+    void delete(Long bookingId);
+
+    void clear();
+}
