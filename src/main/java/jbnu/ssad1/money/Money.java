bd46e1a92755762ee496e5e34863405f42c5f055
Geonu Kim <kgo000926@gmail.com>
2023-12-10 17:53:44 +0900

    [#6] fix: Money 객체간 비교 수정

--- src/main/java/jbnu/ssad1/money/Money.java
+++ src/main/java/jbnu/ssad1/money/Money.java
@@ -1,6 +1,7 @@
 package jbnu.ssad1.money;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Money {
 
@@ -26,7 +27,25 @@ public class Money {
         return new Money(this.money.subtract(other.money));
     }
 
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Money money1 = (Money) object;
+        return Objects.equals(money.doubleValue(), money1.money.doubleValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
+    }
+
     public Money times(double percent) {
         return new Money(this.money.multiply(BigDecimal.valueOf(percent)));
+    }
+
+    @Override
+    public String toString() {
+        return money.toString() + "W";
     }
 }
